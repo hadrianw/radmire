5970fc3d76c0804297ebc5aa5385b5565f65a14c
Hadrian Węgrzynowski <queight@gmail.com>
2011-04-20 13:50:53 +0200

    Correct var args macro usage.

--- utils.h
+++ utils.h
@@ -1,10 +1,10 @@
 #include <stdarg.h>
 #include <assert.h>
 
-#define LOG_FATAL(fmt, ...) rr_fatal(__FILE__, __LINE__, __ASSERT_FUNCTION, fmt, ## __VA_ARGS__)
-#define LOG_ERROR(fmt, ...) rr_error(__FILE__, __LINE__, __ASSERT_FUNCTION, fmt, ## __VA_ARGS__)
-#define LOG_WARN(fmt, ...) rr_warn(__FILE__, __LINE__, __ASSERT_FUNCTION, fmt, ## __VA_ARGS__)
-#define LOG_INFO(fmt, ...) rr_info(fmt, ## __VA_ARGS__)
+#define LOG_FATAL(fmt, ...) rr_fatal(__FILE__, __LINE__, __ASSERT_FUNCTION, fmt, __VA_ARGS__)
+#define LOG_ERROR(fmt, ...) rr_error(__FILE__, __LINE__, __ASSERT_FUNCTION, fmt, __VA_ARGS__)
+#define LOG_WARN(fmt, ...) rr_warn(__FILE__, __LINE__, __ASSERT_FUNCTION, fmt, __VA_ARGS__)
+#define LOG_INFO(fmt, ...) rr_info(fmt, __VA_ARGS__)
 
 void rr_fatal(const char *file, int line, const char *function, const char *format, ...);
 void rr_error(const char *file, int line, const char *function, const char *format, ...);
