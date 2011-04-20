a9500c49d997f19a2743d763ebdc1bf25eff2e83
Hadrian Węgrzynowski <queight@gmail.com>
2011-04-20 15:11:47 +0200

    Add more transform and vector math.

--- rr_math.h
+++ rr_math.h
@@ -1,6 +1,20 @@
 #include "rr_types.h"
 #include <math.h>
 
+#ifdef RR_DOUBLE_FLOAT
+#define rr_sqrt sqrt
+#define rr_sin sin
+#define rr_cos cos
+#define rr_asin asin
+#define rr_acos acos
+#else
+#define rr_sqrt srtf
+#define rr_sin sinf
+#define rr_cos cosf
+#define rr_asin asinf
+#define rr_acos acosf
+#endif
+
 static inline unsigned int to_pow2(unsigned int v)
 {
         v--;
@@ -33,12 +47,55 @@ static inline RRfloat rr_vec2_sqlen(struct RRvec2 v)
         return v.x*v.x + v.y*v.y;
 }
 
+static inline RRfloat rr_vec2_len(struct RRvec2 v)
+{
+        return rr_sqrt(rr_vec2_sqlen(v));
+}
+
 static inline struct RRvec2 rr_transform_vect(const struct RRtransform t,
                 const struct RRvec2 v)
 {       
         struct RRvec2 res;
 	res.x = t.pos.x+t.col1.x*v.x+t.col2.x*v.y;
 	res.y = t.pos.y+t.col1.y*v.x+t.col2.y*v.y;
+
+	return res;
+}
+
+static inline struct RRtransform rr_transform_mul(const struct RRtransform a,
+                const struct RRtransform b)
+{       
+        struct RRtransform res;
+	res.col1.x = a.col1.x * b.col1.x + a.col2.x * b.col1.y;
+	res.col2.x = a.col1.x * b.col2.x + a.col2.x * b.col2.y;
+	res.pos.x = a.col1.x * b.pos.x + a.col2.x * b.pos.y + a.pos.x;
+
+	res.col1.y = a.col1.y * b.col1.x + a.col2.y * b.col1.y;
+	res.col2.y = a.col1.y * b.col2.x + a.col2.y * b.col2.y;
+	res.pos.y = a.col1.y * b.pos.x + a.col2.y * b.pos.y + a.pos.y;
+	return res;
+}
+
+static inline void rr_transform_set_angle(const struct RRtransform *t,
+                RRfloat angle)
+{       
+        RRfloat c = rr_cos(angle);
+        RRfloat s = rr_sin(angle);
+	t->col1.x = c; t->col2.x = -s;
+	t->col1.y = s; t->col2.y = c;
+
+	return res;
+}
+
+static inline struct RRtransform rr_transform_from_vec2(const struct RRvec2 v)
+{       
+        struct RRtransform res;
+        RRfloat len = rr_vec2_len(v);
+        RRfloat c = v.x / len;
+        RRfloat s = v.y / len;
+
+        res.col1.x = c; res.col2.x = -s; res.pos.x = len;
+        res.col1.y = s; res.col2.y = c; res.pos.y = 0;
 
 	return res;
 }
