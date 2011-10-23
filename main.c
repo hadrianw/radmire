fc238c54aa7f0975a80b86719dff1ad75b52008c
Hadrian Węgrzynowski <queight@gmail.com>
2011-10-23 21:56:54 +0200

    Update example - main.c

--- main.c
+++ main.c
@@ -2,12 +2,23 @@
 #include <stdio.h>
 #include "rr.h"
 #include "rr_math.h"
+#include "rr_image.h"
 
 int main(int argc, char **argv)
 {
         if(rr_init(argc, argv)) {
                 return -1;
         }
+
+        SDL_Surface *s = rrimg_load("square.png");
+        SDL_Surface *z = rrimg_display_format(s);
+        SDL_FreeSurface(s);
+        s = z;
+        glEnable(GL_TEXTURE_2D);
+        unsigned int handle = rrtex_load(s);
+        printf("handle %d\n", handle);
+        SDL_FreeSurface(s);
+
         struct RRvec2 screen_mouse;
         struct RRvec2 screen_mouse_rel;
         while(rr_running) {
@@ -32,6 +43,7 @@ int main(int argc, char **argv)
                         rrgl_draw_arrays(GL_POINTS, rr_root_node, 1);
                 }*/
 
+                /*
                 glColor4ub(0xFF, 0xFF, 0xFF, 0x80);
                 glBegin(GL_LINE_STRIP);
                 glVertex2i(-100, -100);
@@ -46,6 +58,21 @@ int main(int argc, char **argv)
                 glVertex2i(100, 0);
                 glVertex2i(0, -100);
                 glVertex2i(0, 100);
+                glEnd();
+                */
+
+                glActiveTexture(handle);
+                glBegin(GL_QUADS);
+                glColor3ub(0xFF, 0xFF, 0);
+                glTexCoord2f(0, 1);
+                glVertex2i(-100, -100);
+                glColor3ub(0xFF, 0xFF, 0xFF);
+                glTexCoord2f(0, 0);
+                glVertex2i(-100, 100);
+                glTexCoord2f(1, 0);
+                glVertex2i(100, 100);
+                glTexCoord2f(1, 1);
+                glVertex2i(100, -100);
                 glEnd();
 
                 rr_end_scene();
