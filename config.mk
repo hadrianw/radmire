f0366187f1727bed94a4743c70cd60438d8e6c96
Hadrian Węgrzynowski <queight@gmail.com>
2012-02-28 21:44:23 +0100

    Add debug flags to makefile.

--- config.mk
+++ config.mk
@@ -10,6 +10,8 @@ LIBS = `pkg-config --libs sdl SDL_image gl glu` -lphysfs
 CPPFLAGS = #-D
 CFLAGS   = -std=c99 -pedantic -Os -Wall ${INCS} ${CPPFLAGS} -MMD
 LDFLAGS  = -s ${LIBS}
+#CFLAGS   = -std=c99 -pedantic -O0 -g -pg -Wall ${INCS} ${CPPFLAGS} -MMD
+#LDFLAGS  = -pg ${LIBS}
 
 # compiler and linker
 CC = cc
