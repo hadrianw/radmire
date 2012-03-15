066614b8746a8214a7e718cddd39b6b30680b66f
Hadrian Węgrzynowski <queight@gmail.com>
2012-03-15 22:10:01 +0100

    Add some example code with use of rr_array.

--- main.c
+++ main.c
@@ -6,6 +6,15 @@
 #include "rr_image.h"
 #include "rr_array.h"
 
+struct Object {
+        struct RRtransform t;
+        struct RRvec2 s;
+};
+
+struct RRArray objects = {
+        .size = sizeof(struct Object)
+};
+
 int main(int argc, char **argv)
 {
         if(rr_init(argc, argv)) {
@@ -13,15 +22,28 @@ int main(int argc, char **argv)
         }
 
         unsigned int handle = rrteximg_load("square.png");
-
+        
         while(rr_running) {
                 rr_begin_frame();
                 if(rr_pressed_keys[SDLK_ESCAPE])
                         rr_running = false;
+                if(rr_changed_buttons[0] && rr_pressed_buttons[0]) {
+                        struct Object new = {
+                                rr_transform_identity,
+                                {5, 5}
+                        };
+                        new.t.pos = rr_abs_screen_mouse;
+                        rrarray_push(&objects, &new);
+                }
+
                 rr_begin_scene();
 
                 rrgl_bind_texture(handle);
-                rrgl_draw_rect(&rr_abs_screen_mouse, 0);
+                struct Object *p = objects.ptr;
+                for(int i = 0; i < objects.nmemb; i++) {
+                        rrgl_load_transform(&p[i].t);
+                        rrgl_draw_rect(&p[i].s, 0);
+                }
 
                 rr_end_scene();
                 rr_end_frame();
