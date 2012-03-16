027b81de4d92c2bd7b514c6d48f8d58301662348
Hadrian Wegrzynowski <hadrian@hawski.com>
2012-03-16 10:30:31 +0100

    Add LDFLAGS for OSX. Set debug flags as default.

--- config.mk
+++ config.mk
@@ -4,13 +4,15 @@ SRCS := $(wildcard *.c)
 # includes and libs
 INCS = `pkg-config --cflags sdl SDL_image`
 LIBS = `pkg-config --libs sdl SDL_image gl glu` -lphysfs
+# OSX:
+#LIBS = `pkg-config --libs sdl SDL_image` -lphysfs -framework OpenGL
 
 # flags
 CPPFLAGS = #-D
-CFLAGS   = -std=c99 -pedantic -Os -Wall ${INCS} ${CPPFLAGS} -MMD
-LDFLAGS  = -s ${LIBS}
-#CFLAGS   = -std=c99 -pedantic -O0 -g -pg -Wall ${INCS} ${CPPFLAGS} -MMD
-#LDFLAGS  = -pg ${LIBS}
+#CFLAGS   = -std=c99 -pedantic -Os -Wall ${INCS} ${CPPFLAGS} -MMD
+#LDFLAGS  = -s ${LIBS}
+CFLAGS   = -std=c99 -pedantic -O0 -g -pg -Wall ${INCS} ${CPPFLAGS} -MMD
+LDFLAGS  = -pg ${LIBS}
 
 # compiler and linker
 CC = cc
