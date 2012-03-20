ea7ad74b17e316140823cc0755be9b233d13b8a5
Hadrian Wegrzynowski <hadrian@hawski.com>
2012-03-20 20:05:23 +0100

    Move _SDL_main_h define to preprocessor flags.

--- atlas/config.mk
+++ atlas/config.mk
@@ -3,10 +3,10 @@ INCS = `pkg-config --cflags sdl SDL_image`
 LIBS = `pkg-config --libs-only-L --libs-only-l sdl SDL_image`
 
 # flags
-CPPFLAGS = #-D
+CPPFLAGS = -D_SDL_main_h
 #CFLAGS   = -std=c99 -pedantic -Os -Wall ${INCS} ${CPPFLAGS} -MMD
 #LDFLAGS  = -s ${LIBS}
-CFLAGS   = -std=c99 -pedantic -O0 -g -pg -Wall ${INCS} ${CPPFLAGS} -MMD -D_SDL_main_h
+CFLAGS   = -std=c99 -pedantic -O0 -g -pg -Wall ${INCS} ${CPPFLAGS} -MMD
 LDFLAGS  = -pg ${LIBS}
 
 # compiler and linker
