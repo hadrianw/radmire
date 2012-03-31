43a408f3eb7dbad8dce67630b7ff1518b79b07b9
Hadrian Wegrzynowski <hadrian@hawski.com>
2012-03-31 15:24:50 +0200

    Add windows cflags/libs to atlas Makefile. Remove profiler cflag.

--- atlas/config.mk
+++ atlas/config.mk
@@ -4,11 +4,15 @@ LIBS = `pkg-config --libs-only-L sdl` \
        `pkg-config --libs-only-l sdl` \
        `pkg-config --libs libpng`
 
+#### Win
+#INCS = -I../../SDL/include/
+#LIBS = -lmingw32 -L../../SDL/lib/ -lSDL -lpng -lz
+
 # flags
 CPPFLAGS = -D_SDL_main_h
 #CFLAGS   = -std=c99 -pedantic -Os -Wall ${INCS} ${CPPFLAGS}
 #LDFLAGS  = -s ${LIBS}
-CFLAGS   = -std=c99 -pedantic -O0 -g -pg -Wall ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -O0 -g -Wall ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # compiler and linker
